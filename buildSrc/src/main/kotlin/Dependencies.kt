38e26b3c5e0e45495203a43f8ffa50954c282ca9
Caleb-Komen <calebkipyatich@gmail.com>
2022-07-14 00:31:18 +0300

    Dependency management with buildSrc + Kotlin

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,15 @@
+object Dependencies {
+    const val core = "androidx.core:core-ktx:${Versions.core}"
+    const val composeUi = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.compose}"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+    const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+
+    const val junit4 = "junit:junit:${Versions.junit}"
+    const val testExtJunit= "androidx.test.ext:junit:${Versions.testExtJunit}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val composeUiTestJunit4 = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    const val composeUiTestManifest = "androidx.compose.ui:ui-test-manifest:${Versions.compose}"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val core = "1.8.0"
+    const val compose = "1.3.0-alpha01"
+    const val activityCompose = "1.5.0"
+    const val lifecycle = "2.5.0"
+    const val junit = "4.13.2"
+    const val testExtJunit = "1.1.3"
+    const val espresso = "3.4.0"
+}
