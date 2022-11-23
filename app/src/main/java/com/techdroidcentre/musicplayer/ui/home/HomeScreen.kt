2a67770fa0b4dae6f60cece7c5990ca5ff5c024f
Caleb-Komen <calebkipyatich@gmail.com>
2022-11-23 17:06:27 +0300

    Refactor HomeScreen UI.

--- app/src/main/java/com/techdroidcentre/musicplayer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/techdroidcentre/musicplayer/ui/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -37,10 +39,11 @@ fun HomeScreen(
     }
     val mediaItems by homeViewModel.mediaItems.observeAsState()
 
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
         Text(
             text = "Library",
-            style = MaterialTheme.typography.h6
+            style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
         BrowsableItems(
@@ -73,30 +76,31 @@ fun HomeScreenItem(
     title: String,
     modifier: Modifier = Modifier
 ) {
-    Log.d("MainActivity", "----- HomeScreenItem: $coverArt -----")
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.fillMaxWidth()
-    ) {
-        Image(
-            painter = rememberAsyncImagePainter(Uri.parse(coverArt)),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(56.dp)
-        )
-        Text(
-            text = title,
-            style = MaterialTheme.typography.subtitle1,
-            modifier = Modifier
-                .padding(horizontal = 8.dp)
-                .weight(1F)
-        )
-        Icon(
-            imageVector = Icons.Default.KeyboardArrowRight,
-            contentDescription = null,
-            modifier = Modifier.padding(end = 8.dp)
-        )
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(Uri.parse(coverArt)),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(56.dp)
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1F)
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+        Divider()
     }
 }
 
